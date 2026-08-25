2ad20c97d01107d82e8d1a4830a4ba8c4d984bc8
Asharib Kamal <34808132+asharibkamal1@users.noreply.github.com>
2026-08-25 11:44:28 +0500

    Register persistence and lead capture services

--- src/AsharibKamal.Infrastructure/DependencyInjection.cs
+++ src/AsharibKamal.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using AsharibKamal.Application.Abstractions;
 using AsharibKamal.Infrastructure.Persistence;
+using AsharibKamal.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,10 +12,13 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("PortfolioDb");
-        if (!string.IsNullOrWhiteSpace(connectionString))
+        if (string.IsNullOrWhiteSpace(connectionString))
         {
-            services.AddDbContextFactory<PortfolioDbContext>(options => options.UseSqlServer(connectionString));
+            connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=AsharibKamalPortfolio;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
         }
+
+        services.AddDbContextFactory<PortfolioDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddScoped<ILeadCaptureService, LeadCaptureService>();
 
         return services;
     }
