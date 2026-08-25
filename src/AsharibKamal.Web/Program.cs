48b0cfd233e88fc8b521d3e19fd7e26a89e7fed3
Asharib Kamal <34808132+asharibkamal1@users.noreply.github.com>
2026-08-25 11:44:41 +0500

    Enable local portfolio database and SEO endpoints

--- src/AsharibKamal.Web/Program.cs
+++ src/AsharibKamal.Web/Program.cs
@@ -1,7 +1,9 @@
 using AsharibKamal.Application.Abstractions;
 using AsharibKamal.Application.Services;
 using AsharibKamal.Infrastructure;
+using AsharibKamal.Infrastructure.Persistence;
 using AsharibKamal.Web.Components;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,7 @@ builder.Services.AddScoped<IPortfolioService, PortfolioService>();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddResponseCompression();
 builder.Services.AddOutputCache();
+builder.Services.AddAntiforgery();
 
 var app = builder.Build();
 
@@ -22,11 +25,37 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<PortfolioDbContext>>();
+    await using var db = await dbFactory.CreateDbContextAsync();
+    await db.Database.EnsureCreatedAsync();
+}
+
 app.UseHttpsRedirection();
 app.UseResponseCompression();
 app.UseStaticFiles();
 app.UseAntiforgery();
 app.UseOutputCache();
+
+app.MapGet("/robots.txt", () => Results.Text("User-agent: *\nAllow: /\nSitemap: /sitemap.xml", "text/plain"));
+app.MapGet("/sitemap.xml", () => Results.Text("""
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>/</loc></url>
+  <url><loc>/about</loc></url>
+  <url><loc>/experience</loc></url>
+  <url><loc>/projects</loc></url>
+  <url><loc>/blog</loc></url>
+  <url><loc>/services</loc></url>
+  <url><loc>/mentoring</loc></url>
+  <url><loc>/resources</loc></url>
+  <url><loc>/products</loc></url>
+  <url><loc>/newsletter</loc></url>
+  <url><loc>/resume</loc></url>
+  <url><loc>/contact</loc></url>
+</urlset>
+""", "application/xml"));
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
